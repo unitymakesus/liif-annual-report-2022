662fcf05281e2cf5bcc11bd9af60dd85802a9641
CamJam <cjetton18@gmail.com>
2023-02-23 10:52:26 -0500

    add UB back to head

--- source/_partials/head.blade.php
+++ source/_partials/head.blade.php
@@ -6,7 +6,7 @@
   <title>{{ $page->title }}</title>
   <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
   <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
-  {{-- <script>
+  <script>
     window.Userback = window.Userback || {};
     Userback.access_token = '10960|78225|G0cCbrFH9G7QCSQUl6rYqBu3e';
     (function(d) {
@@ -14,5 +14,5 @@
         s.src = 'https://static.userback.io/widget/v1.js';
         (d.head || d.body).appendChild(s);
     })(document);
-  </script> --}}
+  </script>
 </head>
