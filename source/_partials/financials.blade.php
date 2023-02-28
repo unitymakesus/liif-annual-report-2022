a14f8d127cfca15ff6352724dc86624cb804f5e0
CamJam <cjetton18@gmail.com>
2023-02-28 09:35:54 -0500

    fix green rule at bottom of articles section

--- source/_partials/financials.blade.php
+++ source/_partials/financials.blade.php
@@ -1,6 +1,6 @@
 <div class="financials bg-blue-dark text-green-lime">
   <div class="wrapper">
-      <h2 class="font-bold text-4xl green-rule__tabs">
+      <h2 class="font-bold text-4xl green-rule__tabs-alt">
           <span class="block max-w-[1024px] mx-auto">Consolidated Financial Statements</span>
       </h2>
       <div class="max-w-[1024px] mx-auto">
